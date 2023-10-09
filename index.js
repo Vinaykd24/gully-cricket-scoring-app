6dc832495bd78fe15d515981cc92ee3f4e5c58f1
Vinaykd24 <vinay.kadam24@gmail.com>
2023-10-09 17:50:55 -0400

    Moved files to root

---
+++ index.js
@@ -0,0 +1,59 @@
+let score = 0;
+let balls = 0;
+let overs = 0;
+let remainingOvers = 0;
+let ballByBall = [];
+function updateOversAndBalls() {
+  const oversInput = document.getElementById("overs-input");
+  if (oversInput) {
+    overs = parseFloat(oversInput.value);
+    balls = 0;
+    updateOversDisplay();
+  }
+}
+function increaseScoreByRuns(runs) {
+  score += runs;
+  ballByBall.push(runs);
+  updateScoreDisplay();
+  recordBall();
+}
+function addExtras(extra) {
+  score++;
+  updateScoreDisplay();
+}
+function recordBall() {
+  balls++;
+  const totalBallsInAnOver = 6;
+  const totalBallsPlayed = overs * totalBallsInAnOver - balls;
+  const _remainingOvers = Math.floor(totalBallsPlayed / totalBallsInAnOver);
+  const remainingBalls = totalBallsPlayed % totalBallsInAnOver;
+  remainingOvers = parseFloat(`${_remainingOvers}.${remainingBalls}`);
+  updateOversDisplay();
+}
+function updateScoreDisplay() {
+  const scoreElement = document.getElementById("score");
+  if (scoreElement) {
+    scoreElement.textContent = score.toString();
+  }
+}
+function updateOversDisplay() {
+  const oversElement = document.getElementById("overs");
+  const remainingOversElement = document.getElementById("remainingOvers");
+  if (oversElement) {
+    oversElement.textContent = `${overs}`;
+  }
+  if (remainingOversElement) {
+    remainingOversElement.textContent = `${remainingOvers}`;
+  }
+}
+document.getElementById("increase-by-1")?.addEventListener("click", () => increaseScoreByRuns(1));
+document.getElementById("increase-by-2")?.addEventListener("click", () => increaseScoreByRuns(2));
+document.getElementById("increase-by-3")?.addEventListener("click", () => increaseScoreByRuns(3));
+document.getElementById("increase-by-4")?.addEventListener("click", () => increaseScoreByRuns(4));
+document.getElementById("increase-by-5")?.addEventListener("click", () => increaseScoreByRuns(5));
+document.getElementById("increase-by-6")?.addEventListener("click", () => increaseScoreByRuns(6));
+document.getElementById("wideBall")?.addEventListener("click", () => addExtras("wide"));
+document.getElementById("noBall")?.addEventListener("click", () => addExtras("noBall"));
+document.getElementById("update-overs-button")?.addEventListener("click", updateOversAndBalls);
+updateScoreDisplay();
+updateOversDisplay();
